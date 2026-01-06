dc5b4078706f2d1b97f5bd8172c4c9162ed461b1
CodeBySnehaSrishti <srishtisneha10@gmail.com>
2026-01-05 23:08:51 -0800

    added 5th project

---
+++ 6-modal/script.js
@@ -0,0 +1,6 @@
+function openModal(productId) {
+    document.getElementById('modal' + productId).style.display = 'flex';
+  }
+  function closeModal(productId) {
+    document.getElementById('modal' + productId).style.display = 'none';
+  }
